7fd0f4b03dc5c74a3adea1726ac71e4f7fe98a89
xubzero <allannjugush@gmail.com>
2021-04-01 09:19:11 +0300

    Nothing important

--- ringzero/web/7.anothercaptch.py
+++ ringzero/web/7.anothercaptch.py
@@ -10,16 +10,13 @@ import lxml.html as parser
 session=requests.Session()
 #Website details
 host='http://challenges.ringzer0team.com:10142'
-phpsession='PHPSESSID=fvsfauegs0sb8eippd57gfhhc6'
-# phpsession=phpsession+';flag=1; __gads=ID=e75162129edcde5f-220a81e4a2a600b5:T=1610651720:RT=1610651720:S=ALNI_MZYo4etZq3fymohq5Rho9ZelSlEBw; gadsTest=test; _ga=GA1.2.206858975.1610651719; _gid=GA1.2.519857400.1612793617'
-custom_headers={"Cookie":phpsession+';wp_woocommerce_session_7a281fb6a05a19671ea88de2bb4f0399=b7459c0d9ca386ea9966b870a3d7fd87||1612969616||1612966016||7f36141938dda67003b05de0019c15a7'}
 
 
 
 url=host
 
 
-response=session.get(url,headers=custom_headers)
+response=session.get(url)
 content=response.text
 status=response.status_code
 headers=response.headers
@@ -27,14 +24,16 @@ cookies=response.cookies
 
 # Getting the source code link and the contents of the source code
 sourcecode=host+'/captcha3.txt'
-# print(sourcecode)
 # print(session.get(sourcecode,headers=custom_headers).text)
 
 
 
 
 #Sending the payload 
-payload='{"captcha":"nothing"}'
+sessionvalue=""
+phpsession=f'PHPSESSID={sessionvalue}'
+custom_headers={"Cookie":phpsession+';wp_woocommerce_session_7a281fb6a05a19671ea88de2bb4f0399=b7459c0d9ca386ea9966b870a3d7fd87||1612969616||1612966016||7f36141938dda67003b05de0019c15a7'}
+payload={"captcha":sessionvalue}
 new_response=(session.post('http://challenges.ringzer0team.com:10142/captcha3.php',headers=custom_headers,data=payload))
 new_content=new_response.text
 print(new_content)
